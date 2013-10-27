7e47c184bd6e01d3e7678a7f4ab600f751f1f77f
cmcrae <codymcrae@gmail.com>
2013-10-28 04:04:22 +1100

    Added README

--- MetropolisHastings/metropolis_hastings.py
+++ MetropolisHastings/metropolis_hastings.py
@@ -1,6 +1,33 @@
 import numpy as np
 from numpy.random import rand
 
+def metropolis_step(x,y,logP):
+    '''
+    Performs a single Metropolis update given the 
+    current x and the proposal y. This is just
+    Metropolis-Hastings with a symmetric proposal distribution
+    i.e. Q(x|y) = Q(y|x)
+
+    Parameters
+    ----------
+    x : scalar or array
+        Current state of Markov chain.
+    y : scalar or array
+        Proposal drawn from the proposal distribution, Q(.|x)
+    logP(x) : Function
+        Computes log P(x)  i.e. the log probability target
+        distribution P at x
+    '''
+
+    # Calculate the log of the Metropolis ratio 
+    log_ratio = logP(y) - logP(x)
+
+    # Accept/reject step
+    if np.isfinite(log_ratio) and np.log(rand()) < log_ratio:
+        return y
+    else:
+        return x
+
 def metropolis_hastings_step(x,y,logQ,logP):
     '''
     Performs a single Metropolis Hastings update 
@@ -25,33 +52,6 @@ def metropolis_hastings_step(x,y,logQ,logP):
 
     # Accept/reject step
     if isfinite(log_ratio) and log(rand()) < log_ratio:
-        return y
-    else:
-        return x
-
-def metropolis_step(x,y,logP):
-    '''
-    Performs a single Metropolis update given the 
-    current x and the proposal y. This is just
-    Metropolis-Hastings with a symmetric proposal distribution
-    i.e. Q(x|y) = Q(y|x)
-
-    Parameters
-    ----------
-    x : scalar or array
-        Current state of Markov chain.
-    y : scalar or array
-        Proposal drawn from the proposal distribution, Q(.|x)
-    logP(x) : Function
-        Computes log P(x)  i.e. the log probability target
-        distribution P at x
-    '''
-
-    # Calculate the log of the Metropolis ratio 
-    log_ratio = logP(y) - logP(x)
-
-    # Accept/reject step
-    if np.isfinite(log_ratio) and np.log(rand()) < log_ratio:
         return y
     else:
         return x
---
+++ README.md
@@ -0,0 +1,20 @@
+## Code Descriptions
+
+### DPGMM 
+C implementation of the Dirichlet Process Gaussian Mixture Model for the old faithful geyser dataset. See [1] for details regarding MCMC sampling of Dirichlet process mixture models. Information about Dirichlet Process Gaussian Mixture Models can be found in [2].
+
+### IsingModel
+Interactive simulation of the 2D Ising model using the Metropolis algorithm.  A derivation of the Metropolis sampler for the Ising model can be found in Chapter IV Section 31 of [3].
+
+### SliceSample
+1-dimensional slice sampler for a multi-modal target distribution. For information on the slice sampler please see [4].
+
+### MetropolisHastings
+Python implementation of both Metropolis and Metropolis-Hastings algorithm. The example code plots 200 iterations of the (Gaussian) random walk Metropolis sampler under a variety of initial configurations and step sizes. A mixture of two bivariate Normal distributions is used as the target distribution.
+
+1. Neal, R. M. (2000). Markov chain sampling methods for Dirichlet process mixture models. Journal of computational and graphical statistics, 9(2), 249-265.
+2. Görür, D., & Rasmussen, C. E. (2010). Dirichlet process Gaussian mixture models: Choice of the base distribution. Journal of Computer Science and Technology, 25(4), 653-664.
+3. MacKay, D. J. (2003). Information theory, inference and learning algorithms. Cambridge university press.
+4. Neal, R. M. (2003). Slice sampling. Annals of statistics, 705-741.
+
+
